9c9fee07f2428ad064c05adc1facd867ea45f5e6
Jeon, Bong Su <73727736+outinletter@users.noreply.github.com>
2026-09-12 18:52:09 +0900

    Change bold font from HelveticaBold to TimesBold

--- js/pdf-engine.js
+++ js/pdf-engine.js
@@ -553,7 +553,7 @@ async function runHL(){
     const pdfLibDoc=await PDFLib.PDFDocument.load(pdfBytes,{ignoreEncryption:true});
     const libPages=pdfLibDoc.getPages();
     const stdFont = await pdfLibDoc.embedFont(PDFLib.StandardFonts.Courier);
-    const boldFont = await pdfLibDoc.embedFont(PDFLib.StandardFonts.HelveticaBold);
+    const boldFont = await pdfLibDoc.embedFont(PDFLib.StandardFonts.TimesBold);
 
     const BOOKMARK_PATTERNS=[
       {label:'CFP PLAN',pattern:/CFP\s+PLAN/i},
